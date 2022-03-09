9aaf330b066ca8f0e799066fa8d2b37e69d2940c
Giulia Rossatto Rocha <giulia.rocha@unesp.br>
2022-03-09 17:12:39 -0300

    Correção questão 1

--- fibonacci_questao1.py
+++ fibonacci_questao1.py
@@ -2,9 +2,13 @@ import sys
 
 def fibonacci (n):
     if (n==0 or n==1):
-        return n
+        return n    
     return fibonacci(n-1) + fibonacci(n-2)
 
 # Programa principal
-resposta = fibonacci (int(sys.argv[1]))
-print (resposta)
+n = int(sys.argv[1])
+if (n>=0):
+    resposta = fibonacci (n)
+    print (resposta)
+else:
+    print ("Entrada incorreta. Digite um numero positivo.")
